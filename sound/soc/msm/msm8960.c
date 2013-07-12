45000dbfe0ce2b992e4832b26bcd2928478c3d15
dhacker29 <davidhackerdvm@gmail.com>
2013-07-12 01:20:47 -0500

    Revert "mmi: Disable low-latency audio"
    This reverts commit cacb85b735d0af818f964729b1418c2e66181e4d.

--- sound/soc/msm/msm8960.c
+++ sound/soc/msm/msm8960.c
@@ -1478,7 +1478,6 @@ static struct snd_soc_dai_link msm8960_dai_common[] = {
 		.codec_name = "snd-soc-dummy",
 		.be_id = MSM_FRONTEND_DAI_VOICE2,
 	},
-#if 0
 	{
 		.name = "MSM8960 LowLatency",
 		.stream_name = "MultiMedia5",
@@ -1494,7 +1493,6 @@ static struct snd_soc_dai_link msm8960_dai_common[] = {
 		.ignore_pmdown_time = 1,
 		.be_id = MSM_FRONTEND_DAI_MULTIMEDIA5,
 	},
-#endif
 	/* Backend BT/FM DAI Links */
 	{
 		.name = LPASS_BE_INT_BT_SCO_RX,
